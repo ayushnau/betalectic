ef9c03bca3aa6bb6bc095de5e47a384643f4af14
Ayush2020012016 <ayush.2020012016@tulas.edu.in>
2022-07-01 12:38:41 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,8 @@ const home = () => {
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet" />
 
       </Head>
+
+    <div><a href="/teams">teams</a></div>
     </>
   )
 }
