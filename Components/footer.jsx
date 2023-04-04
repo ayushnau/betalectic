78998f25e904fb8a0b0e66053d3c5da407e0b472
Ayush20200012016 <ayush2020012016@tulas.edu.in>
2023-04-04 10:57:11 +0530

    added the changes

--- Components/footer.jsx
+++ Components/footer.jsx
@@ -8,68 +8,120 @@ import linkedin from '../static/linkedin.png'
 
 const footer = () => {
     return (
-        <div className="masterContainer relative text-slate-400 bg-slate-900 font-gothamnarrow-light py-16 bg-[url('https://betalectic.com/utouch/img/clouds.png')]">
-            <div className="w-full md:w-3/4 flex flex-wrap md:flex-nowrap px-4 mx-auto justify-center">
-                <div className="firstsection w-full  md:w-5/12 pr-4 ">
-                    <div className='font-gothamnarrow-black text-5xl mb-8 text-white'>Betaelectic</div>
-                    <p className="font-bold leading-snug">We think beta version of an idea makes or breaks an entrepreneur. That is what we intend to deliver – a minimum viable product, developed on ‘ship-centric’ principles.</p>
-                    <p className="font-bold leading-snug mt-2">In world of open source software, saas and innumerable technology stacks – we figure out what is best for your product (cost wise, development wise, scaling wise) and give you the beta.</p>
-                    <p className="mt-8 font-bold">
-                        <Link href="/"><a className='text-white'>Learn more about Betaelectic</a></Link>
-                    </p>
-
-                </div>
-                <div className="secondsection w-full ml-auto pt-2 md:w-2/12">
-                    <div className="heading font-gothamnarrow-black text-2xl mb-8 text-white">
-                        Useful Links
-                    </div>
-                    <ul className='font-bold'>
-                        <li>Home</li>
-                        <li>About us</li>
-                        <li>Contact</li>
-                    </ul>
-                </div>
-                <div className="thirdsection ml-auto w-full pt-2 md:w-3/12">
-                    <div className="heading font-gothamnarrow-black text-2xl mb-8 text-white">Contact us</div>
-                    <div className="font-bold flex align-middle">
-
-                        <div className="w-7 h-7 inline-block relative rounded-full mr-1">
-                            <Image alt='pic' src={message} className="rounded-full" layout='fill' objectFit='cover' />
-                        </div>
-
-                        <div className="flex-1">contact@betaelectic.com</div></div>
-                    <div className="font-bold flex align-middle">follow us
-                            <Link href="/">
-                                <a>
-                                    <div className="w-7 h-7 inline-block relative rounded-full mr-1">
-                                        <Image alt='pic'  src={"https://img.icons8.com/color/48/000000/linkedin.png"} className="rounded-full" layout='fill' objectFit='cover' />
-                                    </div>
-                                </a>
-                            </Link>
-                            <Link  href="/">
-                                <a>
-                                    <div className="w-7 h-7 inline-block relative rounded-full mr-1">
-                                        <Image  alt='pic' src={"https://img.icons8.com/color/48/000000/twitter-squared.png"} className="rounded-full" layout='fill' objectFit='cover' />
-                                    </div>
-                                </a>
-                            </Link>
-                       
-                    </div>
-                </div>
+      <div className="masterContainer relative text-slate-400 bg-slate-900 font-gothamnarrow-light py-16 bg-[url('https://betalectic.com/utouch/img/clouds.png')]">
+        <div className="w-full md:w-3/4 flex flex-wrap md:flex-nowrap px-4 mx-auto justify-center">
+          <div className="firstsection w-full  md:w-5/12 pr-4 ">
+            <div className="font-gothamnarrow-black text-5xl mb-8 text-white">
+              Betaelectic
             </div>
-            <Link href="/">
+            <p className="font-bold leading-snug">
+              We think beta version of an idea makes or breaks an entrepreneur.
+              That is what we intend to deliver – a minimum viable product,
+              developed on ‘ship-centric’ principles.
+            </p>
+            <p className="font-bold leading-snug mt-2">
+              In world of open source software, saas and innumerable technology
+              stacks – we figure out what is best for your product (cost wise,
+              development wise, scaling wise) and give you the beta.
+            </p>
+            <p className="mt-8 font-bold">
+              <Link href="/">
+                <a className="text-white">Learn more about Betaelectic</a>
+              </Link>
+            </p>
+          </div>
+          <div className="secondsection w-full ml-auto pt-2 md:w-2/12">
+            <div className="heading font-gothamnarrow-black text-2xl mb-8 text-white">
+              Useful Links
+            </div>
+            <ul className="font-bold">
+              <Link href="/aboutus">
                 <a>
-                    <div className="w-16  h-16 hover:shadow-lg hover:shadow-blue-700  rounded-full absolute -bottom-7 left-1/2 -translate-x-1/3">
-                        <Image alt='pic' src={Upward} className="rounded-full" layout='fill' objectFit='cover' />
-
-                    </div>
+                  <li>About us</li>
                 </a>
-            </Link>
-            <div className="text-black font-bold font-gothamnarrow-bold copywright absolute -bottom-16 left-1/2  -translate-x-1/3">
-                copywright &#169; 2020 Betalectic
+              </Link>
+              <Link href="/team">
+                <a>
+                  <li>Team</li>
+                </a>
+              </Link>
+              <Link href="/contact">
+                <a>
+                  <li>Contact</li>
+                </a>
+              </Link>
+            </ul>
+          </div>
+          <div className="thirdsection ml-auto w-full pt-2 md:w-3/12">
+            <div className="heading font-gothamnarrow-black text-2xl mb-8 text-white">
+              Contact us
             </div>
+            <div className="font-bold flex align-middle">
+              <div className="w-7 h-7 inline-block relative rounded-full mr-1">
+                <Image
+                  alt="pic"
+                  src={message}
+                  className="rounded-full"
+                  layout="fill"
+                  objectFit="cover"
+                />
+              </div>
+
+              <div className="flex-1">contact@betaelectic.com</div>
+            </div>
+            <div className="font-bold flex align-middle">
+              follow us
+              <Link href="/">
+                <a>
+                  <div className="w-7 h-7 inline-block relative rounded-full mr-1">
+                    <Image
+                      alt="pic"
+                      src={
+                        "https://img.icons8.com/color/48/000000/linkedin.png"
+                      }
+                      className="rounded-full"
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  </div>
+                </a>
+              </Link>
+              <Link href="/">
+                <a>
+                  <div className="w-7 h-7 inline-block relative rounded-full mr-1">
+                    <Image
+                      alt="pic"
+                      src={
+                        "https://img.icons8.com/color/48/000000/twitter-squared.png"
+                      }
+                      className="rounded-full"
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  </div>
+                </a>
+              </Link>
+            </div>
+          </div>
         </div>
-    )
+        <Link href="/">
+          <a>
+            <div className="w-16  h-16 hover:shadow-lg hover:shadow-blue-700  rounded-full absolute -bottom-7 left-1/2 -translate-x-1/3">
+              <Image
+                alt="pic"
+                src={Upward}
+                className="rounded-full"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+          </a>
+        </Link>
+        <div className="text-black font-bold font-gothamnarrow-bold copywright absolute -bottom-16 left-1/2  -translate-x-1/3">
+          copywright &#169; 2020 Betalectic
+        </div>
+      </div>
+    );
 }
 
 export default footer
--- Components/navbar.jsx
+++ Components/navbar.jsx
@@ -1,59 +1,88 @@
-import React, { useEffect, useState } from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import logo from '../static/betalectic_logo.png'
-
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../static/betalectic_logo.png";
 
 const Navbar = () => {
-  const [navdisplay, setnavdisplay] = useState({ display: 'inline-block' })
+  const [navdisplay, setnavdisplay] = useState({ display: "inline-block" });
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth < '639') {
-        setnavdisplay({ display: 'none' })
-        console.log(window.innerWidth)
+      if (window.innerWidth < "639") {
+        setnavdisplay({ display: "none" });
+        console.log(window.innerWidth);
+      } else {
+        console.log("a");
+        setnavdisplay({ display: "inline-block" });
       }
-      else {
-        console.log("a")
-        setnavdisplay({ display: 'inline-block' })
-      }
-    }
-
+    };
 
     window.addEventListener("resize", handleResize);
-  }, [])
+  }, []);
 
   const handleClick = () => {
-    if (navdisplay.display == 'inline-block') {
-      setnavdisplay({ display: 'none' })
+    if (navdisplay.display == "inline-block") {
+      setnavdisplay({ display: "none" });
+    } else {
+      setnavdisplay({ display: "inline-block" });
     }
-    else {
-      setnavdisplay({ display: 'inline-block' })
-    }
-  }
+  };
 
   return (
-
     <>
       <div className="header relative font-gothamnarrow-medium z-50">
-      <div className="forpsedoelement"></div>
-        <div className='flex flex-wrap  p-2rem relative top-0  text-navbar uppercase font-bold items-center justify-between w-full md:w-3/4 mx-auto'>
-
-          <div className="logo w-75 h-22 ">{<Image alt="pic" className='w-full h-full object-cover' src={logo} />}</div>
-          <div className=' border-black'>
-            <ul style={navdisplay} className='absolute bg-white top-36 left-0 sm:relative sm:top-0 sm:left-0 text-lg shadow-black shadow-lg w-full sm:shadow-none'>
-              <li className='sm:inline-block group block my-4 relative border-black border-2 sm:border-none whitespace-nowrap mx-4 rounded  px-2 py-1'><Link href="/aboutus">
-                <a className='group-hover:before:w-full before:duration-500 before:content-[""] before:w-0 before:h-1 before:bg-black before:absolute before:bottom-0 before:left-0 before:hover '>About Us</a></Link></li>
-              <li className='sm:inline-block group block my-4 relative border-black border-2 sm:border-none whitespace-nowrap mx-4 rounded  px-2 py-1'><Link href="/team">
-                <a className='group-hover:before:w-full before:duration-500 before:content-[""] before:w-0 before:h-1 before:bg-black before:absolute before:bottom-0 before:left-0 before:hover '>Team</a></Link></li>
-              <li className='sm:inline-block group block my-4 relative border-black border-2 sm:border-none whitespace-nowrap mx-4 rounded  px-2 py-1'><Link href="/ourwork">
-                <a className='group-hover:before:w-full before:duration-500 before:content-[""] before:w-0 before:h-1 before:bg-black before:absolute before:bottom-0 before:left-0 before:hover '>Our Work</a></Link></li>
-              <li className='sm:inline-block group block my-4 relative border-black border-2 sm:border-none whitespace-nowrap mx-4 rounded  px-2 py-1'><Link href="/contactus">
-                <a className='group-hover:before:w-full before:duration-500 before:content-[""] before:w-0 before:h-1 before:bg-black before:absolute before:bottom-0 before:left-0 before:hover '>ContactUs</a></Link></li>
-
+        <div className="forpsedoelement"></div>
+        <div className="flex flex-wrap  p-2rem relative top-0  text-navbar uppercase font-bold items-center justify-between w-full md:w-3/4 mx-auto">
+          <div className="logo w-75 h-22 ">
+            <Link href="/aboutus">
+              <a>
+                <Image
+                  alt="pic"
+                  className="w-full h-full object-cover"
+                  src={logo}
+                />
+              </a>
+            </Link>
+          </div>
+          <div className=" border-black">
+            <ul
+              style={navdisplay}
+              className="absolute bg-white top-36 left-0 sm:relative sm:top-0 sm:left-0 text-lg shadow-black shadow-lg w-full sm:shadow-none"
+            >
+              <li className="sm:inline-block group block my-4 relative border-black border-2 sm:border-none whitespace-nowrap mx-4 rounded  px-2 py-1">
+                <Link href="/aboutus">
+                  <a className='group-hover:before:w-full before:duration-500 before:content-[""] before:w-0 before:h-1 before:bg-black before:absolute before:bottom-0 before:left-0 before:hover '>
+                    About Us
+                  </a>
+                </Link>
+              </li>
+              <li className="sm:inline-block group block my-4 relative border-black border-2 sm:border-none whitespace-nowrap mx-4 rounded  px-2 py-1">
+                <Link href="/team">
+                  <a className='group-hover:before:w-full before:duration-500 before:content-[""] before:w-0 before:h-1 before:bg-black before:absolute before:bottom-0 before:left-0 before:hover '>
+                    Team
+                  </a>
+                </Link>
+              </li>
+              <li className="sm:inline-block group block my-4 relative border-black border-2 sm:border-none whitespace-nowrap mx-4 rounded  px-2 py-1">
+                <Link href="/ourwork">
+                  <a className='group-hover:before:w-full before:duration-500 before:content-[""] before:w-0 before:h-1 before:bg-black before:absolute before:bottom-0 before:left-0 before:hover '>
+                    Our Work
+                  </a>
+                </Link>
+              </li>
+              <li className="sm:inline-block group block my-4 relative border-black border-2 sm:border-none whitespace-nowrap mx-4 rounded  px-2 py-1">
+                <Link href="/contactus">
+                  <a className='group-hover:before:w-full before:duration-500 before:content-[""] before:w-0 before:h-1 before:bg-black before:absolute before:bottom-0 before:left-0 before:hover '>
+                    ContactUs
+                  </a>
+                </Link>
+              </li>
             </ul>
-            <div onClick={handleClick} className='hamburger mx-4 flex sm:hidden justify-end'>
-              <span className='text-3xl'>Menu</span>
-              <svg viewBox="0 0 100 80" width="40" height="40" fill='#657c98'>
+            <div
+              onClick={handleClick}
+              className="hamburger mx-4 flex sm:hidden justify-end"
+            >
+              <span className="text-3xl">Menu</span>
+              <svg viewBox="0 0 100 80" width="40" height="40" fill="#657c98">
                 <rect width="100" height="15"></rect>
                 <rect y="30" width="100" height="15"></rect>
                 <rect y="60" width="100" height="15"></rect>
@@ -63,8 +92,7 @@ const Navbar = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
-
+export default Navbar;
