ee2704e98c08b856f392bd0bd631fffc86c1e64d
Ayush20200012016 <ayush2020012016@tulas.edu.in>
2023-04-04 10:47:35 +0530

    added the out work page

---
+++ pages/ourwork.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Ourwork = () => {
+  return (
+    <div className='h-[80vh] font-gothamnarrow-bold flex justify-center items-center text-3xl' >Sorry Not available right now, will be available soon.</div>
+  )
+}
+
+export default Ourwork
